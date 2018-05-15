b180954bae6e34575ea8f78176dfedbc87a586ad
Meena Jim George <meena_jim@yahoo.com>
2018-05-15 14:47:53 +1000

    updates

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
   root :to =>'pages#home'
-  # root :to => 'instructors#index'
 
 
   resources :users, :only => [:new, :create]
-  get '/login' => 'session#new'
-  post '/login' => 'session#create'
-  delete '/login' => 'session#destroy'
+  
+    get '/login' => 'session#new'
+    post '/login' => 'session#create'
+    delete '/login' => 'session#destroy'
+
   resources :instructors do
     post 'like' => 'likes#create'
     delete 'like' => 'likes#destroy'
   end
-  #post '/instructors/:instructor_id/like' => 'likes#create'
   resources :students
   resources :performances
 
---
+++ fix.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+
+OLD_EMAIL="meena@Meenas-MacBook-Pro.local"
+CORRECT_NAME="Meena"
+CORRECT_EMAIL="meena_jim@yahoo.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
